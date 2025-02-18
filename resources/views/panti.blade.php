38151ad819c3d68813d7687e68112f548bf3bbb7
Ashahra Aprilia Puspaanggraini <ashahraaprilia@gmail.com>
2025-02-18 10:54:48 +0700

    benerin punya edna

--- resources/views/panti.blade.php
+++ resources/views/panti.blade.php
@@ -271,14 +271,8 @@
                     <div class="flex justify-between items-center p-3 rounded-lg bg-white">
                         <div>
                             <h3 class="text-lg font-bold text-green-800">${user.name}</h3>
-<<<<<<< HEAD
                             <p class="text-gray-600"><strong>Orphanage Name:</strong> ${user.id}</p>
                             <p class="text-gray-600"><strong>Total Donation:</strong> ${user.email}</p>
-                            ${deleteMode ? `<input type='checkbox' class='delete-checkbox mt-2' data-id='${user.id}'>` : ''}
-=======
-                            <p class="text-gray-600"><strong>ID:</strong> ${user.id}</p>
-                            <p class="text-gray-600"><strong>Email:</strong> ${user.email}</p>
->>>>>>> 7701d2e7a91f302a631a811605f059b5be1beeb5
                         </div>
                         <div class="relative">
                             ${deleteMode 
