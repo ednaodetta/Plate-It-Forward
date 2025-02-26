5bcd81fbe2a82f23ca58b140e0715004c6fa5d21
shirleymargaretta29 <shirleymargaretta029@gmail.com>
2025-02-26 11:02:19 +0700

    plis

--- resources/views/components/html.blade.php
+++ resources/views/components/html.blade.php
@@ -11,15 +11,16 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="stylesheet" href="resources/css/app.css">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-…." crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <!-- Styles / Scripts -->
     @stack('styles') {{-- Untuk tambahan CSS dari halaman lain --}}
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))) --}}
+
+    {{-- @else
         <style>
             /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
             *,
@@ -908,7 +909,7 @@
                 }
             }
         </style>
-    @endif
+    @endif --}}
 
 </head>
 
